853d58836eeeeb07b3cd017774f9d3348dc5b73c
alekseyshmat <leha.2409@mail.ru>
2018-04-28 07:39:12 +0300

    Revert "PPViS_lab1"
    This reverts commit 0298fc55274cbd535f24733ec5dc9b21433e66a7.

--- src/com/company/FirstTask.java
+++ src/com/company/FirstTask.java
@@ -5,23 +5,23 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FirstTask {
-    public JPanel panel = new JPanel();
+public class FirstTask{
+    public  JPanel panel = new JPanel();
     public JPanel addPanel = new JPanel();
     private JTextField text = new JTextField();
     private JComboBox box = new JComboBox();
     private JButton button = new JButton("OK");
 
     public void First() {
-        panel.setPreferredSize(new Dimension(330, 70));
-        text.setPreferredSize(new Dimension(120, 30));
-        box.setPreferredSize(new Dimension(80, 30));
-        button.setPreferredSize(new Dimension(120, 30));
+        panel.setPreferredSize(new Dimension( 330,70));
+        text.setPreferredSize(new Dimension(120,30));
+        box.setPreferredSize(new Dimension(80,30));
+        button.setPreferredSize(new Dimension(120,30));
 
         addPanel.add(text);
         addPanel.add(box);
         addPanel.add(button);
-        panel.add(addPanel);
+      //  panel.add(addPanel);
 
         button.addActionListener(new ActionListener() {
             @Override
@@ -30,6 +30,7 @@ public class FirstTask {
                     for (int j = 0; j < box.getItemCount(); j++)
                         if (text.getText().equals(box.getItemAt(j))) {
                             JOptionPane.showMessageDialog(null, "Такой объект уже есть ", "Внимание", JOptionPane.PLAIN_MESSAGE);
+                            System.out.println(box.getItemAt(j));
                             return;
                         }
                 }
--- src/com/company/ForthTask.java
+++ src/com/company/ForthTask.java
@@ -5,17 +5,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ForthTask {
-    public JPanel panel = new JPanel();
-    public JPanel checkPanel = new JPanel();
+public class ForthTask{
+    public  JPanel panel = new JPanel();
+    private JPanel checkPanel = new JPanel();
     private JTextField text4 = new JTextField();
     private JButton button4 = new JButton("Start");
     private JCheckBox checkBox1 = new JCheckBox("1");
     private JCheckBox checkBox2 = new JCheckBox("2");
     private JCheckBox checkBox3 = new JCheckBox("3");
 
-    public void Forth() {
-        panel.setPreferredSize(new Dimension(400, 70));
+    public void Forth(){
+        panel.setPreferredSize(new Dimension( 400,70));
         text4.setPreferredSize(new Dimension(120, 30));
         button4.setPreferredSize(new Dimension(100, 30));
 
@@ -29,16 +29,17 @@ public class ForthTask {
         button4.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!text4.getText().isEmpty()) {
-                    if (text4.getText().equals(checkBox1.getText()))
+                if(!text4.getText().isEmpty()) {
+                    if(text4.getText().equals(checkBox1.getText()))
                         checkBox1.doClick();
-                    else if (text4.getText().equals(checkBox2.getText()))
+                    else if(text4.getText().equals(checkBox2.getText()))
                         checkBox2.doClick();
-                    else if (text4.getText().equals(checkBox3.getText()))
+                    else if(text4.getText().equals(checkBox3.getText()))
                         checkBox3.doClick();
                     else
                         JOptionPane.showMessageDialog(null, "Такого имени не существует ", "Внимание", JOptionPane.PLAIN_MESSAGE);
-                } else
+                }
+                else
                     JOptionPane.showMessageDialog(null, "Введите текст! ", "Внимание", JOptionPane.PLAIN_MESSAGE);
             }
         });
